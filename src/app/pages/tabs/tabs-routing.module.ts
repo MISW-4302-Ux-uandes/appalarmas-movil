d15db00dcd439376ba86d5a5bbab13c57ef6f445
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2023-03-08 18:30:53 -0500

    Se crea proyecto Ionic

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'alarmas',
+        loadChildren: () => import('../alarmas/alarmas.module').then( m => m.AlarmasPageModule)
+      },
+      {
+        path: 'grupos',
+        loadChildren: () => import('../grupos/grupos.module').then( m => m.GruposPageModule)
+      },
+      {
+        path: 'categorias',
+        loadChildren: () => import('../categorias/categorias.module').then( m => m.CategoriasPageModule)
+      },
+      {
+        path: 'ajustes',
+        loadChildren: () => import('../ajustes/ajustes.module').then( m => m.AjustesPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/alarmas',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/alarmas',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+})
+export class TabsPageRoutingModule {}
