68f086ddce8c0311b17281e9501d696f555ae0a2
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2023-03-09 07:49:15 -0500

    Se crea componente header

--- src/app/pages/ajustes/ajustes.module.ts
+++ src/app/pages/ajustes/ajustes.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { AjustesPageRoutingModule } from './ajustes-routing.module';
 
 import { AjustesPage } from './ajustes.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AjustesPageRoutingModule
+    AjustesPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [AjustesPage]
 })
--- src/app/pages/grupos/grupos.module.ts
+++ src/app/pages/grupos/grupos.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { GruposPageRoutingModule } from './grupos-routing.module';
 
 import { GruposPage } from './grupos.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    GruposPageRoutingModule
+    GruposPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [GruposPage]
 })
