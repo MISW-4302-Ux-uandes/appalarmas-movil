ec4c5a25aaea1ee5e42c5c0aafc8d06705b6c28e
nbarreto80235 <nbarreto@uniandes.edu.co>
2023-03-10 18:36:50 -0500

    Ajuste vista temas

--- src/app/pages/ajustes-tema/ajustes-tema.page.ts
+++ src/app/pages/ajustes-tema/ajustes-tema.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ajustes-tema',
@@ -8,13 +9,42 @@ import { ModalController } from '@ionic/angular';
 })
 export class AjustesTemaPage implements OnInit {
 
-  constructor(private _modalCtrl:ModalController) { }
+  constructor(private _modalCtrl:ModalController, private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
   cerrarModal() {
     this._modalCtrl.dismiss()
+  }
+
+  async luzAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Clic',
+      message: 'Modo luz',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async oscuroAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Clic',
+      message: 'Modo oscuro',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+  async configAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Clic',
+      message: 'Configuración sistema',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
 }
