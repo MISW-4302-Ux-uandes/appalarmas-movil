01fa8fb2f343542afab5fd5986ee9795494a4b71
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2023-03-10 10:30:11 -0500

    Se implementa formulario para crear alarma sencilla

--- src/app/pages/modal-form-alarma-sencilla/modal-form-alarma-sencilla.page.ts
+++ src/app/pages/modal-form-alarma-sencilla/modal-form-alarma-sencilla.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-form-alarma-sencilla',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalFormAlarmaSencillaPage implements OnInit {
 
-  constructor() { }
+
+  constructor(private _fb: FormBuilder, private _modalCtrl:ModalController) { }
+
+  formAlarmaSencilla: FormGroup = this._fb.group({
+    titulo:[,[Validators.required]],
+    fecha: [, [Validators.required]],
+    hora:[,[Validators.required]],
+    frecuencia:[,[Validators.required]],
+    tono:[,[Validators.required]],
+    volSonido:[50,Validators.required],
+    notas:[]
+  })
 
   ngOnInit() {
+  }
+
+  crearAlarmaSencilla(){
+    console.log('Submit: ',this.formAlarmaSencilla.value)
+    if(this.formAlarmaSencilla.invalid){
+      this.formAlarmaSencilla.markAllAsTouched()
+      return
+    }
+  }
+
+  campoValido(campo:string){
+    return this.formAlarmaSencilla.controls[campo].errors && this.formAlarmaSencilla.controls[campo].touched
+  }
+
+  cancelar(){
+    this._modalCtrl.dismiss();
+  }
+
+  mostrarCalendario(){
+    console.log('Show modal')
+  }
+
+  mostrarTiempo(){
+    console.log('Show hora')
   }
 
 }
