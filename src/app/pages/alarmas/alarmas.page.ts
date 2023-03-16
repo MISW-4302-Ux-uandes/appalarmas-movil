89c337aa56b789919a49ac80943d06158b2b8553
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2023-03-15 19:18:05 -0500

    Se implement slides en el home para proximas alarmas

--- src/app/pages/alarmas/alarmas.page.ts
+++ src/app/pages/alarmas/alarmas.page.ts
@@ -7,7 +7,12 @@ import { ModalAlarmaParaPage } from '../modal-alarma-para/modal-alarma-para.page
 import { ModalAlarmasHoyPage } from '../modal-alarmas-hoy/modal-alarmas-hoy.page';
 import { ModalListaAlarmasPage } from '../modal-lista-alarmas/modal-lista-alarmas.page';
 
-
+interface Alarma{
+  titulo:string;
+  categoria:string;
+  fecha:string;
+  activo:boolean
+}
 @Component({
   selector: 'app-alarmas',
   templateUrl: './alarmas.page.html',
@@ -15,6 +20,42 @@ import { ModalListaAlarmasPage } from '../modal-lista-alarmas/modal-lista-alarma
 })
 export class AlarmasPage implements OnInit {
 
+  alarmSlide = {
+    slidesPerView: 1.1,
+  }
+
+  alarmas:Alarma[]=[
+    {
+        titulo: "Entrega Ux",
+        categoria:"Recordatorio rápido",
+        fecha:"11:59 2023-03-15",
+        activo: true
+    },
+    {
+        titulo: "Parcial Nube",
+        categoria:"Alarma sencilla",
+        fecha:"11:59 2023-03-22",
+        activo: true
+    },
+    {
+        titulo: "Odontología",
+        categoria:"Cita médica",
+        fecha:"09:20 2023-04-22",
+        activo: true
+    },
+    {
+        titulo: "Hipertensión",
+        categoria:"Toma medicamentos",
+        fecha:"09:20 2023-04-22",
+        activo: true
+    },
+    {
+        titulo: "Reunion Sprint",
+        categoria:"Recordatorio rápido",
+        fecha:"15:15 2023-03-24",
+        activo: true
+    }
+]
   constructor(private _modalCtrl: ModalController,
               private _alertas: AlertasService,) { }
 
@@ -58,6 +99,13 @@ export class AlarmasPage implements OnInit {
       component: GruposFormularioPage
     });
     await modalCrearGrupo.present();
+  }
+
+  toggleChange(event:any){
+    const checked = event.detail.checked;
+    if(!checked){
+      this._alertas.alertConfirmation('¿Seguro que desea desactiva esta alarma?','Alarma desactivada')
+    }
   }
 
 }
